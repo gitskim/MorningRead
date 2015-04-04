2a1ec5eb5c402dea6a17a76d548645cf7e9124e1
Suhyun Kim <logicdevildotcom@gmail.com>
2015-04-04 15:42:32 -0400

    Removed duplicate file.

--- app/src/main/java/com/logicdevil/morningread/api/response/ChildrenData.java
+++ app/src/main/java/com/logicdevil/morningread/api/response/ChildrenData.java
@@ -1,67 +1,84 @@
 package com.logicdevil.morningread.api.response;
 
+import com.google.gson.annotations.Expose;
+
 /**
  * Created by suhyunkim on 3/15/15.
  */
 public class ChildrenData extends BaseResponse{
 
-    String domain;
+
+    @Expose
+    private String domain;
+
+    @Expose
+    private String subreddit;
+
+    @Expose
+    private String title;
+
+    @Expose
+    private String name;
 
     public String getDomain() {
         return domain;
     }
 
+    public String getSubreddit() {
+        return subreddit;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getName() {
+        return name;
+    }
+
     @Override
     public String toString() {
-        return "Response3Data{" +
+        return "Response4Content{" +
                 "domain='" + domain + '\'' +
+                ", subreddit='" + subreddit + '\'' +
+                ", title='" + title + '\'' +
+                ", name='" + name + '\'' +
                 '}';
     }
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
 
         ChildrenData that = (ChildrenData) o;
 
-        if (domain != null ? !domain.equals(that.domain) : that.domain != null) return false;
+        if (domain != null ? !domain.equals(that.domain) : that.domain != null) {
+            return false;
+        }
+        if (name != null ? !name.equals(that.name) : that.name != null) {
+            return false;
+        }
+        if (subreddit != null ? !subreddit.equals(that.subreddit) : that.subreddit != null) {
+            return false;
+        }
+        if (title != null ? !title.equals(that.title) : that.title != null) {
+            return false;
+        }
 
         return true;
     }
 
     @Override
     public int hashCode() {
-        return domain != null ? domain.hashCode() : 0;
+        int result = domain != null ? domain.hashCode() : 0;
+        result = 31 * result + (subreddit != null ? subreddit.hashCode() : 0);
+        result = 31 * result + (title != null ? title.hashCode() : 0);
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
     }
-//    @Expose
-//    private Response4Content content;
-//
-//    public Response4Content getContent() {
-//        return content;
-//    }
-//
-//    @Override
-//    public String toString() {
-//        return "Response3Data{" +
-//                "content=" + content +
-//                '}';
-//    }
-//
-//    @Override
-//    public boolean equals(Object o) {
-//        if (this == o) return true;
-//        if (o == null || getClass() != o.getClass()) return false;
-//
-//        Response3Data that = (Response3Data) o;
-//
-//        if (content != null ? !content.equals(that.content) : that.content != null) return false;
-//
-//        return true;
-//    }
-//
-//    @Override
-//    public int hashCode() {
-//        return content != null ? content.hashCode() : 0;
-//    }
 }
